996beb9bf914efd5a14efa4129396e4d9de039a1
PadjTheGoat <146662613+YacineIdir@users.noreply.github.com>
2025-09-22 08:30:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ gitConfigFunc() {
 }
 
 downloadPack() {
-  sudo pacman -Sy nvim git gcc gdb python python-pip
+  sudo pacman -Sy nvim git gcc gdb python python-pip nmap
+  sudo yay -S matroschka openpuff pngcheck silenteye stegcracker stegdetect steghide stegolego stegoveritas stegseek stegsolve stepic zsteg
   git clone https://github.com/LazyVim/starter ~/.config/nvim
   rm -rf ~/.config/nvim/.git
 }
